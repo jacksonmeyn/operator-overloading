bac2b8b15fdd069ba5be441c6531dd1c5feefaed
jacksonmeyn <30762914+jacksonmeyn@users.noreply.github.com>
2018-02-03 15:30:47 +1000

    Initial commit

---
+++ SongDuration/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongDuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Test the constructor by entering some invalid parameters");
+            SongDuration invalidDuration1 = new SongDuration(-5, 43);
+            SongDuration invalidDuration2 = new SongDuration(1, 65);
+            SongDuration invalidDuration3 = new SongDuration(20, 59);
+
+            SongDuration song1 = new SongDuration(1, 59);
+            Console.WriteLine(song1.ToString());
+            SongDuration song3 = new SongDuration(20, 0);
+            Console.WriteLine(song3.ToString());
+            SongDuration song4 = new SongDuration(3, 30);
+            Console.WriteLine(song4.ToString());
+            SongDuration song5 = new SongDuration(3, 30);
+            Console.WriteLine(song5.ToString());
+
+            //Test the + and - operators
+            SongDuration calculate = song1 + song4;
+            Console.WriteLine(calculate.ToString());
+            calculate = song4 - song1;
+            Console.WriteLine(calculate.ToString());
+            calculate = song1 - song4;
+            Console.WriteLine(calculate.ToString());
+
+            //Test the == and != operators
+            Console.WriteLine(song4 == song5); //true
+            Console.WriteLine(song4 == song1); //false
+            Console.WriteLine(song4 != song5); //false
+            Console.WriteLine(song4 != song3); //true
+
+            //Test the < and <= operators
+            Console.WriteLine(song4 < song5); //false
+            Console.WriteLine(song4 < song3); //true
+            Console.WriteLine(song4 <= song5); //true
+            Console.WriteLine(song4 <= song1); //false
+
+            //Test the > and >= operators
+            Console.WriteLine(song4 > song5); //false
+            Console.WriteLine(song4 > song3); //false
+            Console.WriteLine(song4 >= song5); //true
+            Console.WriteLine(song4 >= song1); //true
+
+            //Hold the screen
+            Console.ReadLine();
+        }
+    }
+
+    
+}
---
+++ SongDuration/SongDuration.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongDuration
+{
+    class SongDuration
+    {
+        private int minutes;
+        private int seconds;
+
+        public int Minutes { get => minutes; set => minutes = value; }
+        public int Seconds { get => seconds; set => seconds = value; }
+
+        public SongDuration()
+        {
+            Minutes = 0;
+            Seconds = 0;
+        }
+
+        public SongDuration(int _minutes, int _seconds)
+        {
+            if (_minutes < 0 || _minutes > 20 || _minutes == 20 && _seconds > 0)
+            {
+                try
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+            else
+            {
+                Minutes = _minutes;
+            }
+
+            if (_seconds < 0 || _seconds > 59)
+            {
+                try
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Seconds = _seconds;
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} minutes {1} seconds", Minutes, Seconds);
+        }
+
+        //Operator overloading
+        public static bool operator !=(SongDuration a, SongDuration b)
+        {
+            if (a.Minutes == b.Minutes && a.Seconds == b.Seconds)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool operator ==(SongDuration a, SongDuration b)
+        {
+            if (a.Minutes == b.Minutes && a.Seconds == b.Seconds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <=(SongDuration a, SongDuration b)
+        {
+            if (a.Minutes < b.Minutes)
+            {
+                return true;
+            }
+            else if (a.Minutes == b.Minutes && a.Seconds <= b.Seconds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator >=(SongDuration a, SongDuration b)
+        {
+            if (a.Minutes > b.Minutes)
+            {
+                return true;
+            }
+            else if (a.Minutes == b.Minutes && a.Seconds >= b.Seconds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator >(SongDuration a, SongDuration b)
+        {
+            if (a.Minutes > b.Minutes)
+            {
+                return true;
+            }
+            else if (a.Minutes == b.Minutes && a.Seconds > b.Seconds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(SongDuration a, SongDuration b)
+        {
+            if (a.Minutes < b.Minutes)
+            {
+                return true;
+            }
+            else if (a.Minutes == b.Minutes && a.Seconds < b.Seconds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static SongDuration operator +(SongDuration a, SongDuration b)
+        {
+
+            SongDuration tempSong = new SongDuration();
+            tempSong.Minutes = a.Minutes + b.Minutes;
+            tempSong.Seconds = a.Seconds + b.Seconds;
+            if (tempSong.Seconds > 59)
+            {
+                tempSong.Minutes++;
+                tempSong.Seconds -= 60;
+            }
+
+            if (tempSong.Minutes > 20 ||
+                tempSong.Minutes == 20 && tempSong.Seconds > 0)
+            {
+                Console.WriteLine("Error: The result is greater than 20 minutes 0 seconds. Setting to that instead.");
+                tempSong.Minutes = 20;
+                tempSong.Seconds = 0;
+            }
+
+            return tempSong;
+        }
+
+        public static SongDuration operator -(SongDuration a, SongDuration b)
+        {
+
+            SongDuration tempSong = new SongDuration();
+            tempSong.Minutes = a.Minutes - b.Minutes;
+            tempSong.Seconds = a.Seconds - b.Seconds;
+            if (tempSong.Seconds < 0)
+            {
+                tempSong.Minutes--;
+                tempSong.Seconds += 60;
+            }
+
+            if (tempSong.Minutes < 0 || tempSong.Seconds < 0)
+            {
+                Console.WriteLine("Error: The result is less than 0 minutes 0 seconds. Setting to that instead.");
+                tempSong.Minutes = 0;
+                tempSong.Seconds = 0;
+            }
+
+            return tempSong;
+        }
+        //End operator overloading
+
+    }
+}
